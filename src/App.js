a3d4870cee8cb54655ee6cf7d283858afc7418c7
Fernando Ugarte-BDev <fernando.ugarte@bairesdev.com>
2024-08-21 19:00:09 -0400

    Private links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
@@ -16,24 +16,43 @@ import RoutineSeriesByStudent from './components/RoutineSeriesByStudent';
 import RoutineSeriesList from './components/RoutineSeriesList';
 import { auth } from './firebaseConfig';
 
+function PrivateRoute({ element: Component, ...rest }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe(); // Cleanup subscription on unmount
+  }, []);
+
+  if (loading) {
+    return <div>Cargando...</div>; // Muestra un mensaje de carga mientras se verifica la autenticación
+  }
+
+  return currentUser ? <Component {...rest} /> : <Navigate to="/login" />;
+}
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/students" element={<StudentForm />} />
-        <Route path="/student-list" element={<StudentList />} />
-        <Route path="/edit-student/:id" element={<EditStudent />} />
-        <Route path="/exercise-list" element={<ExerciseList />} />
-        <Route path="/add-exercise" element={<AddExercise />} />
-        <Route path="/routine-list" element={<RoutineList />} />
-        <Route path="/add-routine" element={<AddRoutine />} />
-        <Route path="/edit-exercise/:id" element={<EditExercise />} />
-        <Route path="/edit-routine/:id" element={<EditRoutine />} />
-        <Route path="/assign-routine-series/:studentId/:seriesId?" element={<RoutineSeriesByStudent />} />
-        <Route path="/routine-series-list/:studentId" element={<RoutineSeriesList />} />
-        {/* Redirigir la ruta raíz ("/") al Dashboard */}
+        <Route path="/dashboard" element={<PrivateRoute element={Dashboard} />} />
+        <Route path="/students" element={<PrivateRoute element={StudentForm} />} />
+        <Route path="/student-list" element={<PrivateRoute element={StudentList} />} />
+        <Route path="/edit-student/:id" element={<PrivateRoute element={EditStudent} />} />
+        <Route path="/exercise-list" element={<PrivateRoute element={ExerciseList} />} />
+        <Route path="/add-exercise" element={<PrivateRoute element={AddExercise} />} />
+        <Route path="/routine-list" element={<PrivateRoute element={RoutineList} />} />
+        <Route path="/add-routine" element={<PrivateRoute element={AddRoutine} />} />
+        <Route path="/edit-exercise/:id" element={<PrivateRoute element={EditExercise} />} />
+        <Route path="/edit-routine/:id" element={<PrivateRoute element={EditRoutine} />} />
+        <Route path="/assign-routine-series/:studentId/:seriesId?" element={<PrivateRoute element={RoutineSeriesByStudent} />} />
+        <Route path="/routine-series-list/:studentId" element={<PrivateRoute element={RoutineSeriesList} />} />
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </Router>
